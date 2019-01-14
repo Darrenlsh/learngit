36bd233eac3b05805d5f70eab23acdec00b1a4f4
Darrenlsh <2389886145@qq.com>
2019-01-14 20:35:24 +0800

    添加文件上传项目

---
+++ spring-boot-fileupload/src/main/java/com/darren/springbootfileupload/SpringBootFileuploadApplication.java
@@ -0,0 +1,38 @@
+package com.darren.springbootfileupload;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author luosihao
+ */
+@SpringBootApplication
+public class SpringBootFileuploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFileuploadApplication.class, args);
+    }
+
+    /**
+     * Tomcat large file upload connection reset
+     *
+     * tomcatEmbedded这段代码是为了解决，上传文件大于10M出现连接重置的问题。
+     * 此异常内容GlobalException也捕获不到。
+     */
+    @Bean
+    public TomcatServletWebServerFactory tomcatEmbedded() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                //-1 means unlimited
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+        return tomcat;
+    }
+}
+
